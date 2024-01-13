8edf2abffa301e536f5d745a5c5156b0c36cac16
mela <144652524+stormweavering@users.noreply.github.com>
2024-01-12 23:52:41 -0500

    Create struct.cpp

---
+++ struct.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+// Default values for now, will adjust when I have a definite list of each option and the highest character count they take up. Might make test it out with dynamic memory allocation first.
+struct Character{
+char Name[20];
+short int Age;
+char Class[10];
+char Hairstyle[20];
+char HairColor[20];
+char EyeColor[10];
+char NoseShape[10];
+char MouthShape[10];
+char SkinColor[15];
+char Shirt[15];
+char Pants[10];
+char Shoes[10];
+char Accessories[15];
+};
+
+int main(void)
+{
+  return 0;
+}
