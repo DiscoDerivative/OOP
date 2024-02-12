1fcb54a40cb93db88b4021ff5c129d21d9fdbcaf
sai <144652524+saiuji@users.noreply.github.com>
2024-02-12 14:29:53 -0500

    Update Counter.cpp

--- Counter.cpp
+++ Counter.cpp
@@ -1,60 +1,21 @@
-#include <iostream>
-
 class Counter{
-  private:
-  int counter;
-  public:
-  //Intializing counter to 0
-  Counter() : counter(0) {};
-
-  //Pre-fix operator overloading
-  void operator++()
-  {
-    ++counter;
-  }
-
-  void operator--()
-  {
-    --counter;
-  }
-
-  //Post-fix operator overloading
-  void operator++(int)
-  {
-    counter++;
-  }
-
-  void operator--(int)
-  {
-    counter--;
-  }
-
-  int getCounter()
-  {
-    return counter;
-  }
+    private:
+    int count;
+    public:
+    Counter() : count(0){}
+    Counter& operator++(){
+        count += 1;
+        return *this;
+    }
+    int getCount() const{
+        return count;
+    }
 };
 
-
-//Main function
-int main(void)
-{
-  Counter c1, c2, c3, c4;
-  
-  //Prefix
-  ++c1;
-  std::cout << "C1: " << c1.getCounter() << '\n';
-
-  ++c2;
-  --c2;
-  std::cout << "C2: " << c2.getCounter() << '\n';
-
-  c3++;
-  std::cout << "C3: " << c3.getCounter() << '\n';
-  
-  c4++;
-  c4--;
-  std::cout << "C4: " << c4.getCounter() << '\n';
-  
-  return 0;
+#include <iostream>
+int main(void){
+    Counter c1;
+    ++c1;
+    std::cout << "Counter value: " << c1.getCount();
+    return 0;
 }
